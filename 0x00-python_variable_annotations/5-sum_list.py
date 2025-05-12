110841acbcb94ac1312644c6f58b2e7bec676096
xander254 <alexkinyanjui356@gmail.com>
2025-05-13 02:17:13 +0300

    compute the sm of vals in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A module that  takes a list input_list of floats as argument and returns their sum as a float
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    A func that takes a list input_list of floats as argument and 
+    returns their sum as a float
+
+    Args: 
+      float: items in the list
+    Returns:
+        float: sum of items in list
+    """
+    total = sum(input_list)
+    return total
