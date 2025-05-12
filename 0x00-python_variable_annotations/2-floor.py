3cf6a7a08ba5d4a682fc9e17c413d38d6d2833eb
xander254 <alexkinyanjui356@gmail.com>
2025-05-13 01:55:21 +0300

    doc string fix

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -8,6 +8,9 @@ A module that calculates the floor of a float
 def floor(n: float) -> int:
     """
     a func that returns the floor of a float n
+
+    Returns: 
+        int: floor of the argument
     """
     fl = math.floor(n)
     return fl
