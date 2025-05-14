2885969c39e1e9e4d5fc62d9ca0c90671fbfecff
xander254 <alexkinyanjui356@gmail.com>
2025-05-14 17:26:01 +0300

    A func that returns a tuple which is a str and a suare of the second arg

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from typing import Union, Optional, Tuple
+"""
+A module that takes a string and a int/float and returns a tupple
+"""
+
+
+def to_kv(k: str, v: Union[int | float]) -> Tuple[str | float]:
+    """
+    A func that takes a string and a int/float and returns a tuple
+    Args:
+        k: str
+        v: float | int
+    Returns:
+        tup: tuple of k and v
+    """
+    v = v * v
+    tup = (k, v)
+    return tup
