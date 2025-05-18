04034aa4431595cb39db1145a47bcb998bfd2421
xander254 <alexkinyanjui356@gmail.com>
2025-05-18 14:24:32 +0300

    doc string fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,17 +8,14 @@ a module that runs using asyc and random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay 
+    Asynchronous coroutine that waits for a random delay
     between 0 and max_delay seconds.
-
     Args:
         max_delay (int): Maximum number of seconds to wait.
         Default is 10.
-    
     Returns:
         float: The actual number of seconds the coroutine
         waited (randomly chosen).
-    
     This coroutine should be awaited.
     """
     time: float = random.uniform(0, max_delay)
