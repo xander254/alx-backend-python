ada8a21a69298f04969af1276253461749348c8d
xander254 <alexkinyanjui356@gmail.com>
2025-07-13 20:49:28 +0300

    safe elements first

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,7 @@ from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Check safe first"""
     if lst:
         return lst[0]
     else:
