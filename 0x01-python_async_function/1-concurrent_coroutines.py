3e8e494cacb067cf73c16f266a605fb5ae5866c7
xander254 <alexkinyanjui356@gmail.com>
2025-07-13 21:05:06 +0300

    module documentation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-import asyncio
-from typing import List
 """
 concurernt function
 """
+
+
+import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
